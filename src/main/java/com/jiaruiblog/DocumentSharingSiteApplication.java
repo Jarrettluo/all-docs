b45b07bac4b214ee8905ea5bd7019d419b0d771d
Jarrett <291979254@qq.com>
2022-08-06 12:41:16 +0800

    url-pattern 必需符合格式要求

--- src/main/java/com/jiaruiblog/DocumentSharingSiteApplication.java
+++ src/main/java/com/jiaruiblog/DocumentSharingSiteApplication.java
@@ -1,15 +1,8 @@
 package com.jiaruiblog;
 
-//import com.mongodb.client.MongoDatabase;
-//import com.mongodb.client.gridfs.GridFSBucket;
-//import com.mongodb.client.gridfs.GridFSBuckets;
-//import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 import org.springframework.boot.web.servlet.ServletComponentScan;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.data.mongodb.MongoDbFactory;
 
 
 /**
@@ -23,25 +16,9 @@ import org.springframework.boot.web.servlet.ServletComponentScan;
 @SpringBootApplication
 public class DocumentSharingSiteApplication {
 
-
-//    @PostConstruct
-//    public void init() {
-//        // 解决netty启动冲突的问题（主要体现在启动redis和elasticsearch）
-//        // 可以看Netty4Util.setAvailableProcessors(..)
-//        System.setProperty("es.set.netty.runtime.available.processors", "false");
-//    }
-
     public static void main(String[] args) {
         SpringApplication.run(DocumentSharingSiteApplication.class, args);
     }
 
-//    @Autowired
-//    private MongoDbFactory mongoDbFactory;
-//
-//    @Bean
-//    public GridFSBucket getGridFSBuckets() {
-//        MongoDatabase db = mongoDbFactory.getDb();
-//        return GridFSBuckets.create(db);
-//    }
 
 }
--- src/main/java/com/jiaruiblog/filter/JwtFilter.java
+++ src/main/java/com/jiaruiblog/filter/JwtFilter.java
@@ -18,7 +18,7 @@ import java.util.Map;
  */
 @Slf4j
 //@WebFilter(filterName = "JwtFilter", urlPatterns = "/secure/*")
-@WebFilter(filterName = "JwtFilter", urlPatterns = {"/comment/auth/*", "*/auth/*"})
+@WebFilter(filterName = "JwtFilter", urlPatterns = {"/comment/auth/*", "/collect/auth/*", "/document/auth/*"})
 public class JwtFilter implements Filter
 {
 
