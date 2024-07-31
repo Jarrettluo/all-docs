7f5af3f9a33f9fc5450aa5e2ce2cde58dd95cb3a
sshx <sshx@sl.com>
2024-07-31 15:36:05 +0800

    fix_bug:文件上传和文件预览 冲突问题

--- src/main/java/com/jiaruiblog/filter/JwtFilter.java
+++ src/main/java/com/jiaruiblog/filter/JwtFilter.java
@@ -44,13 +44,7 @@ public class JwtFilter implements Filter
         response.setCharacterEncoding("UTF-8");
         String url = request.getRequestURI().substring(request.getContextPath().length());
         // 登录和注册等请求不需要令牌
-//        if (url.contains("login") || url.contains("/user/insert") || url.contains("files")) {
-//            response.setStatus(HttpServletResponse.SC_OK);
-//            chain.doFilter(request, response);
-//            return;
-//        }
-
-        if (url.contains("login") || url.contains("/user/insert")) {
+        if (url.contains("login") || url.contains("/user/insert") || url.contains("view")) {
             response.setStatus(HttpServletResponse.SC_OK);
             chain.doFilter(request, response);
             return;
