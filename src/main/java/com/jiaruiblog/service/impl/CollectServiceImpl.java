aed147b9480f0cb7e1b424bd1440c497f3702517
Jarrett <luojiarui2@163.com>
2025-07-01 17:10:42 +0800

    refactor(CollectServiceImpl): 调整了CollectServiceImpl

--- src/main/java/com/jiaruiblog/service/impl/CollectServiceImpl.java
+++ src/main/java/com/jiaruiblog/service/impl/CollectServiceImpl.java
@@ -3,40 +3,31 @@ package com.jiaruiblog.service.impl;
 import com.jiaruiblog.entity.CollectDocRelationship;
 import com.jiaruiblog.exception.BusinessException;
 import com.jiaruiblog.exception.ErrorCode;
+import com.jiaruiblog.repository.CollectRepository;
 import com.jiaruiblog.service.CollectService;
 import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.query.Criteria;
-import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
 /**
+ * 收藏服务实现类
  * @author Jarrett Luo
- * @Date 2022/6/7 11:40
- * @Version 1.0
  */
 @Slf4j
 @Service
 public class CollectServiceImpl implements CollectService {
 
-    public static final String COLLECTION_NAME = "collectCollection";
-
-    private static final String DOC_ID = "docId";
-
     @Resource
-    MongoTemplate mongoTemplate;
+    CollectRepository collectRepository;
+
 
     /**
-     * @return com.jiaruiblog.utils.ApiResult
-     * @author luojiarui
-     * @Description // 对某个文档进行关注
-     * @Date 9:31 下午 2022/6/23
-     * @Param [collect]
-     **/
+     * 添加文档收藏关系
+     * @param collect 收藏关系实体
+     */
     @Override
     public void insert(CollectDocRelationship collect) {
         Boolean aBoolean = insertRelationShip(collect);
@@ -51,70 +42,48 @@ public class CollectServiceImpl implements CollectService {
         if (collectDb != null) {
             return false;
         }
-        mongoTemplate.save(collect, COLLECTION_NAME);
+        collectRepository.save(collect);
         return true;
     }
 
     /**
-     * @return com.jiaruiblog.utils.ApiResult
-     * @author luojiarui
-     * @Description // 删除收藏关系
-     * @Date 9:43 下午 2022/6/23
-     * @Param [collect]
-     **/
+     * 移除文档收藏关系
+     * @param collect 收藏关系实体
+     */
     @Override
     public void remove(CollectDocRelationship collect) {
-        collect = getExistRelationship(collect);
-        while (collect != null) {
-            mongoTemplate.remove(collect, COLLECTION_NAME);
-            collect = getExistRelationship(collect);
-        }
+        collectRepository.findByDocIdAndUserId(collect.getDocId(), collect.getUserId())
+                .ifPresent(collectRepository::remove);
     }
 
     /**
-     * @return com.jiaruiblog.entity.CollectDocRelationship
-     * @author luojiarui
-     * @Description // 查询已经存在的关系
-     * @Date 9:37 下午 2022/6/23
-     * @Param []
-     **/
+     * 查询已存在的收藏关系
+     * @param collect 收藏关系实体
+     * @return 已存在的收藏关系实体
+     */
     private CollectDocRelationship getExistRelationship(CollectDocRelationship collect) {
         collect = Optional.ofNullable(collect).orElse(new CollectDocRelationship());
-        Query query = new Query()
-                .addCriteria(Criteria.where(DOC_ID).is(collect.getDocId())
-                        .and("userId").is(collect.getUserId()));
-
-        return mongoTemplate.findOne(
-                query, CollectDocRelationship.class, COLLECTION_NAME
-        );
+        return collectRepository.findByDocIdAndUserId(collect.getDocId(), collect.getUserId())
+                .orElse(null);
     }
 
     /**
-     * @return java.lang.Long
-     * @author luojiarui
-     * @Description 查询某个文档下面的点赞数量
-     * @Date 22:35 2022/9/24
-     * @Param [docId]
-     **/
+     * 查询文档收藏数量
+     * @param docId 文档ID
+     * @return 收藏数量
+     */
     @Override
     public Long collectNum(String docId) {
-        Query query = new Query().addCriteria(Criteria.where(DOC_ID).is(docId));
-        return mongoTemplate.count(query, CollectDocRelationship.class, COLLECTION_NAME);
+        return collectRepository.countByDocId(docId);
     }
 
     /**
-     * @author luojiarui
-     * @Description // 根据文档的id删除掉点赞的关系
-     * @Date 11:17 上午 2022/6/25
-     * @Param [docId]
-     **/
+     * 根据文档ID移除所有相关收藏关系
+     * @param docId 文档ID
+     */
     @Override
     public void removeRelateByDocId(String docId) {
-        Query query = new Query(Criteria.where(DOC_ID).is(docId));
-        List<CollectDocRelationship> relationships = mongoTemplate.find(query, CollectDocRelationship.class,
-                COLLECTION_NAME);
+        List<CollectDocRelationship> relationships = collectRepository.findAllByDocId(docId);
         relationships.forEach(this::remove);
     }
-
-
 }
